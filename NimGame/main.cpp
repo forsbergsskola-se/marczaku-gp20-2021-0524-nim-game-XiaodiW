c306257ace540c1b58bbdd61c83916c2457b81a5
Xiaodi Wang <xiaodi@kth.se>
2021-05-24 23:11:22 +0200

    Update NimGame

--- NimGame/main.cpp
+++ NimGame/main.cpp
@@ -3,13 +3,23 @@
 #include <unistd.h>
 #include <string>
 
+std:: string printMatches(int matches){
+    std:: string s(matches, '|');
+    return s;
+}
+
+std:: string printBoundary(){
+    std:: string s(24, '*');
+    return s;
+}
 
 int playerTurn(int match){
-    std::cout << "\n" << "*************************************" << std::endl;
+    std::cout << "\n" << printBoundary() << std::endl;
     int number;
     choose:
     std::cout << "Now, it is Your turn" << std::endl;
     std::cout << "There are " << match << " matches left."<< std::endl;
+    std::cout << printMatches(match) << std::endl;
     std::ostringstream oss;
     oss << "It is your turn, How much do you want to take? ";
     for (int i = 1; i < 4; ++i) if (match >= i) oss << i << ",";
@@ -21,20 +31,22 @@ int playerTurn(int match){
         std::cout << "Please type in accepted number" << std::endl;
         goto choose; }
     std::cout << "You took " << number << " matches. There are "<< match - number <<" left." << std::endl;
-    std::cout << "*************************************" << std::endl;
+    std::cout << printMatches(match - number) << std::endl;
+    std::cout << printBoundary() << std::endl;
     return match - number;
 }
 
 int aiTurn(int match){
-    std::cout << "\n" << "*************************************" << std::endl;
-    std::cout << "Now, it is AI turn" << std::endl;
-    sleep(1);
+    std::cout << "\n" << printBoundary() << std::endl;
+    std::cout << "Now, it is AI's turn" << std::endl;
     std::cout << "AI is thinking....." << std::endl;
-    sleep(3);
+    sleep(2);
     int number = (match-1) % 4;
-    if(number == 0) number = 1;
+    if(number == 0) number = rand() % 3 + 1;
+    if(match == 1) number = 1;
     std::cout << "AI took " << number << " matches. There are "<< match - number <<" left." << std::endl;
-    std::cout << "*************************************" << std::endl;
+    std::cout << printMatches(match - number) << std::endl;
+    std::cout << printBoundary() << std::endl;
     return match - number;
 }
 
@@ -53,9 +65,9 @@ int main() {
             break;
         }
     }
-    std::cout << "\n" << "*************************************" << std::endl;
+    std::cout << "\n" << printBoundary() << std::endl;
     if(isWin) std::cout << "AI took the last match. You WIN! " << std::endl;
     else std::cout << " You took the last match. AI WIN! " << std::endl;
-    std::cout << "*************************************" << std::endl;
+    std::cout << printBoundary() << std::endl;
     return 0;
 }
