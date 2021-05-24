4973156247bc079807d9a3b6987bb693252aa317
Xiaodi Wang <xiaodi@kth.se>
2021-05-24 22:16:22 +0200

    Implement NimGame with AI opponent

--- NimGame/main.cpp
+++ NimGame/main.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 #include <sstream>
-
+#include <unistd.h>
+#include <string>
 
 
 int playerTurn(int match){
+    std::cout << "\n" << "*************************************" << std::endl;
     int number;
     choose:
+    std::cout << "Now, it is Your turn" << std::endl;
     std::cout << "There are " << match << " matches left."<< std::endl;
     std::ostringstream oss;
-    oss << "It is your turn, How much do you want to take?";
-    for (int i = 1; i < 4; ++i) if (match > i) oss << i << ",";
-    oss << "?";
+    oss << "It is your turn, How much do you want to take? ";
+    for (int i = 1; i < 4; ++i) if (match >= i) oss << i << ",";
     std::string str = oss.str();
-    std::cout << str << std::endl;
+    str.pop_back();
+    std::cout << str << "? " << std::endl;
     std::cin >> number;
-    if(number < 1 || number > 3) {
+    if(number < 1 || number > 3 || number > match) {
         std::cout << "Please type in accepted number" << std::endl;
         goto choose; }
-    std::cout << "Player took " << number << " matches. There are "<< match <<" left." << std::endl;
+    std::cout << "You took " << number << " matches. There are "<< match - number <<" left." << std::endl;
+    std::cout << "*************************************" << std::endl;
     return match - number;
 }
 
+int aiTurn(int match){
+    std::cout << "\n" << "*************************************" << std::endl;
+    std::cout << "Now, it is AI turn" << std::endl;
+    sleep(1);
+    std::cout << "AI is thinking....." << std::endl;
+    sleep(3);
+    int number = (match-1) % 4;
+    if(number == 0) number = 1;
+    std::cout << "AI took " << number << " matches. There are "<< match - number <<" left." << std::endl;
+    std::cout << "*************************************" << std::endl;
+    return match - number;
+}
 
 int main() {
     int match = 24;
-    match = playerTurn(match);
-    std::cout << match << std::endl;
+    bool isWin;
+    while (true) {
+        match = playerTurn(match);
+        if(match == 0) {
+            isWin = false;
+            break;
+        }
+        match = aiTurn(match);
+        if(match == 0) {
+            isWin = true;
+            break;
+        }
+    }
+    std::cout << "\n" << "*************************************" << std::endl;
+    if(isWin) std::cout << "AI took the last match. You WIN! " << std::endl;
+    else std::cout << " You took the last match. AI WIN! " << std::endl;
+    std::cout << "*************************************" << std::endl;
     return 0;
 }
